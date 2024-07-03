cdae8ad7d85560bc7a03088243d0c1264eef5cde
djhwanndj <dodgehwang@gmail.com>
2024-07-03 18:31:28 +0900

    CSS가 깨졌어...

--- client/src/containers/partner/PartnerReviewContainer.jsx
+++ client/src/containers/partner/PartnerReviewContainer.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState , useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import * as partners from '../../apis/partner/partner';
 import PartnerReview from '../../components/partner/PartnerReviewList';
+import { LoginContext   } from '../../components/contexts/LoginContextProvider'; // LoginContext만 import
 
 const PartnerReviewContainer = ( {partnerNo} ) => {
+  const { userInfo } = useContext(LoginContext); // 로그인 컨텍스트에서 userInfo 가져오기
   const [partnerReview, setPartnerReview] = useState([]);
   const [isLoading, setLoading] = useState(false)
 
@@ -20,9 +22,12 @@ const PartnerReviewContainer = ( {partnerNo} ) => {
     }
   };
 
+
   useEffect(() => {
-    getPartnerReviews();
-  }, [partnerNo]);
+    if (userInfo  && userInfo .partnerNo) {
+      getPartnerReviews(userInfo .partnerNo); // userInfo에서 partnerNo를 가져와 사용
+    }
+  }, [userInfo ]);
 
   return (
     <PartnerReview reviews={partnerReview} 
