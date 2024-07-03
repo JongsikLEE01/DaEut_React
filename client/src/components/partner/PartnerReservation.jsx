a7bbf98febb997773e3f12e9e423d63baa40e6e9
djhwanndj <dodgehwang@gmail.com>
2024-07-03 18:55:05 +0900

    우수수 깨지는 CSS여...

--- client/src/components/partner/PartnerReservation.jsx
+++ client/src/components/partner/PartnerReservation.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import './css/partner.css';
 
-const PartnerReservation = ({  }) => {
-  
+const PartnerReservation = ({ orderList }) => {
   const toggleSidebar = () => {
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('d-none');
@@ -11,6 +9,7 @@ const PartnerReservation = ({  }) => {
   return (
     <div className="container-fluid container">
       <button className="btn btn-primary toggle-btn menu mt-2 myBtn d-md-none transitionNone" id="toggle-btn" onClick={toggleSidebar}>메뉴</button>
+
       <main className="container-fluid container">
         <div className="row">
           <nav className="col-md-3 col-lg-2 sidebar" id="sidebar">
@@ -24,26 +23,34 @@ const PartnerReservation = ({  }) => {
           </nav>
           <div className="col-md-9 col-lg-10 form-section">
             <h3>예약 관리</h3>
-            <table className="table table-sm table-hover">
-              <thead className="table-light">
-                <tr>
-                  <th width="30">No.</th>
-                  <th width="50">사용자 명</th>
-                  <th width="80">예약 상태</th>
-                  <th width="100">예약 시간</th>
-                  <th width="50">자세히보기</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td data-label="No."></td>
-                  <td data-label="사용자 명"></td>
-                  <td data-label="예약 상태"></td>
-                  <td data-label="예약 시간"></td>
-                  <td data-label="자세히보기"></td>
-                </tr>
-              </tbody>
-            </table>
+            {orderList && orderList.length > 0 ? (
+              <table className="table table-sm table-hover">
+                <thead className="table-light">
+                  <tr>
+                    <th width="30">No.</th>
+                    <th width="50">사용자 명</th>
+                    <th width="80">예약 상태</th>
+                    <th width="100">예약 시간</th>
+                    <th width="50"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orderList.map((order, index) => (
+                    <tr key={order.ordersNo}>
+                      <td data-label="No.">{index + 1}</td>
+                      <td data-label="사용자 명">{order.user.userName}</td>
+                      <td data-label="예약 상태">{order.orderStatus}</td>
+                      <td data-label="예약 시간">{new Date(order.payments.serviceDate).toLocaleString()}</td>
+                      <td data-label="자세히보기">
+                        <a href={`/partner/partnerReservationRead?ordersNo=${order.ordersNo}`} className="detail-button">자세히보기</a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <p>예약이 없습니다.</p>
+            )}
           </div>
         </div>
       </main>
