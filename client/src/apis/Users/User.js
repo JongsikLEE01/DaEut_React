0ee8a3ab7bfc87830b68b6a52b7a513f46322a94
Joono0513 <tbc04156@gmail.com>
2024-07-05 12:33:15 +0900

    마이페이지 유저정보 수정완료

--- client/src/apis/Users/User.js
+++ client/src/apis/Users/User.js
@@ -1,51 +1,20 @@
 import api from '../api';
-import Cookies from 'js-cookie';
 
 export const getUserInfo = () => api.get('/user/userMypage');
 
 export const updateUserInfo = (data) => {
-  const token = Cookies.get('accessToken');
-  if (!token) {
-    console.error('No access token found');
-    return Promise.reject(new Error('No access token found'));
-  }
-
   return api.put('/user/userMypageUpdateDone', data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
+    params: {
+      action: 'update',
     },
   });
 };
 
 export const getUserReservations = () => api.get('/user/userReservation');
 
-export const getUserPaymentsAndReview = () => {
-  const token = Cookies.get('accessToken');
-  if (!token) {
-    return Promise.reject(new Error('No access token found'));
-  }
-
-  return api.get('/user/userReview', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-};
-
-export const submitUserReview = (reviewData) => {
-  const token = Cookies.get('accessToken');
-  if (!token) {
-    console.error('No access token found');
-    return Promise.reject(new Error('No access token found'));
-  }
-
-  return api.post('/user/userReviewDone', reviewData, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-};
-
+export const getUserPaymentsAndReview = () => api.get('/user/userReview')
+ 
+export const submitUserReview = (reviewData) => api.post('/user/userReviewDone', reviewData)
 
 export const getUserChatRooms = () => api.get('/user/userChatRoom')
 
