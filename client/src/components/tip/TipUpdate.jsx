1f36ef0625f55acfa375d4368edb1d8148aee095
kmsjhj07 <kmsjhj07@gmail.com>
2024-07-05 10:56:07 +0900

    10:56

--- client/src/components/tip/TipUpdate.jsx
+++ client/src/components/tip/TipUpdate.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../tip/css/TipRead.module.css';
+import styles from '../tip/css/TipInsert.module.css';
 
 const TipUpdate = ({
   board,
   mainImagePreview,
   additionalImagePreview,
-  fileList,
   handleInputChange,
   handleMainImageChange,
   handleAdditionalImageChange,
   handleSubmit,
-  handleFileDelete,
   handleDeleteConfirm,
+  handleFileDelete,
   boardNo,
+  fileList
 }) => {
   return (
-    <div className="container">
-      <form id="form" onSubmit={handleSubmit} encType="multipart/form-data">
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
+        <div className={`mb-3 d-flex align-items-center rounded px-3 py-2 ${styles['custom-border']}`}>
+          <label htmlFor="userId" className={`form-label mb-0 text-center me-3 ${styles['label-adjust']}`}>작성자 ID</label>
+          <div className={styles.verticalLine}></div>
+          <input type="text" id="userId" className={`form-control ${styles['borderless-input']}`} value={board.userId || ''} disabled />
+        </div>
         <div className="mb-3">
           <label htmlFor="title" className="form-label visually-hidden">제목</label>
           <input
             type="text"
             id="title"
-            className="form-control"
+            className={`form-control ${styles['title-input']}`}
             name="boardTitle"
             value={board.boardTitle}
             onChange={handleInputChange}
@@ -31,7 +36,7 @@ const TipUpdate = ({
         </div>
         <div className="mb-3">
           <textarea
-            className="form-control"
+            className={`form-control ${styles['content-input']}`}
             id="content"
             rows="8"
             name="boardContent"
@@ -39,8 +44,8 @@ const TipUpdate = ({
             onChange={handleInputChange}
           ></textarea>
         </div>
-        <div className="mb-3">
-          <label htmlFor="thumbnail" className="form-label">썸네일</label>
+        <div className="mb-2 mt-5 d-flex align-items-center">
+          <label htmlFor="thumbnail" className={`form-label ${styles['label-adjust']}`}>썸네일</label>
           <input
             type="file"
             className="form-control"
@@ -49,45 +54,47 @@ const TipUpdate = ({
             accept="image/*"
             onChange={handleMainImageChange}
           />
-          <div className="form-text">사진은 최대 20MB 이하의 JPG, PNG, GIF 파일 10장까지 첨부 가능합니다.</div>
-          <div id="image-main-container" className="d-flex flex-wrap gap-2">
-            {mainImagePreview.map((src, index) => (
-              <img key={index} src={src} alt="이미지 미리보기" className="img-thumbnail" style={{ maxWidth: '200px' }} />
-            ))}
-          </div>
+          <label htmlFor="thumbnail" className={`btn btn-outline-secondary ${styles['file-upload-button1']}`}>첨부하기</label>
         </div>
-        <div className="mb-3">
-          <label htmlFor="file" className="form-label">첨부파일</label>
+        <div className="form-text">사진은 최대 20MB 이하의 JPG, PNG, GIF 파일 10장까지 첨부 가능합니다.</div>
+        <div id="image-thumbnail-container" className={`d-flex flex-wrap gap-2 ${styles.thumbnailPreview}`}>
+          {mainImagePreview.map((src, index) => (
+            <img key={index} src={src} alt="이미지 미리보기" className="img-thumbnail" style={{ maxWidth: '200px' }} />
+          ))}
+        </div>
+        <div className="mb-2 mt-5 d-flex align-items-center">
+          <label htmlFor="images" className={`form-label ${styles['label-adjust']}`}>첨부파일</label>
           <input
             type="file"
             className="form-control"
-            id="file"
+            id="images"
             name="file"
             accept="image/*"
             multiple
             onChange={handleAdditionalImageChange}
           />
-          <div className="form-text">사진은 최대 20MB 이하의 JPG, PNG, GIF 파일 10장까지 첨부 가능합니다.</div>
-          <div id="image-preview-container" className="d-flex flex-wrap gap-2">
-            {additionalImagePreview.map((src, index) => (
-              <img key={index} src={src} alt="이미지 미리보기" className="img-thumbnail" style={{ maxWidth: '200px' }} />
-            ))}
-          </div>
+          <label htmlFor="images" className={`btn btn-outline-secondary ${styles['file-upload-button1']}`}>첨부하기</label>
+        </div>
+        <div className="form-text">사진은 최대 20MB 이하의 JPG, PNG, GIF 파일 10장까지 첨부 가능합니다.</div>
+        <div id="image-preview-container" className={`d-flex flex-wrap gap-2 ${styles.thumbnailPreview}`}>
+          {additionalImagePreview.map((src, index) => (
+            <img key={index} src={src} alt="이미지 미리보기" className="img-thumbnail" style={{ maxWidth: '200px' }} />
+          ))}
         </div>
         <div className="mb-3">
           <ul>
             {fileList.map(file => (
               <li key={file.fileNo}>
                 <a href={`/file/img/${file.fileNo}`} download>{file.originFileName}</a>
-                <button type="button" onClick={() => handleFileDelete(file.fileNo)} className="btn btn-primary me-2">삭제</button>
+                <button type="button" onClick={() => handleFileDelete(file.fileNo)} className={`btn ${styles['btn-primary-custom']} me-2`}>삭제</button>
               </li>
             ))}
           </ul>
         </div>
         <div className="d-flex justify-content-end">
-          <button type="submit" className="btn btn-primary me-2 custom-button">수정하기</button>
-          <button type="button" onClick={handleDeleteConfirm} className="btn btn-custom me-2">삭제하기</button>
-          <a href={`/tip/boards/${boardNo}`} className="btn btn-custom-outline">목록</a>
+        <button type="submit" className={`btn ${styles['btn-submit']} me-2`}>수정하기</button>
+          <button type="button" onClick={handleDeleteConfirm} className={`btn ${styles['btn-delete']} me-2`}>삭제하기</button>
+          <a href={`/tip/boards/${boardNo}`} className={`btn ${styles['btn-back']}`}>목록</a>
         </div>
       </form>
     </div>
--- client/src/pages/admin/UserManagementPage.jsx
+++ client/src/pages/admin/UserManagementPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import MainLayout from '../../layouts/MainLayout'
-import AdminUserContainer from '../../containers/Admin/AdminUserContainer'
+import AdminUserContainer from '../../containers/Admin/adminUserContainer'
 
 const UserManagementPage = () => {
   return (
