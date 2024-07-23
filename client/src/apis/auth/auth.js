6984bdade63d533ea7e44901da3e528b82adf37b
downy <tkwk36@naver.com>
2024-07-23 15:50:13 +0900

    플러터 때문에 살짝 고쳤어요

--- client/src/apis/auth/auth.js
+++ client/src/apis/auth/auth.js
@@ -1,77 +1,77 @@
-import axios from 'axios'
-import api from '../api'
+import axios from 'axios';
+import api from '../api';
 
 // 로그인
-export const login = (username, password) => api.post(`/login?username=${username}&password=${password}`)
+export const login = (username, password) => api.post(`/login?username=${username}&password=${password}`);
 
 // 사용자 정보
-export const info = () => api.get(`/info`)
+export const info = () => api.get(`/api/users/info`);
 
 // 회원 가입
-export const join = (data) => api.post(`/join`, data)
+export const join = (data) => api.post(`/api/join`, data);
 
 // 관리자 회원 가입
-export const adminJoin = (data, systemPw) => api.post(`/admin/join`, data, { params: { systemPw }})
+export const adminJoin = (data, systemPw) => api.post(`/api/admin/join`, data, { params: { systemPw }});
 
 // 회원 정보 수정
-export const update = (data) => api.put(`/auth`, data)
+export const update = (data) => api.put(`/api/auth`, data);
 
 // 회원 탈퇴
-export const remove = (userId) => api.delete(`/auth/${userId}`)
+export const remove = (userId) => api.delete(`/api/auth/${userId}`);
 
 // 아이디 중복 확인
 export const checkDuplicateId = (userId) => {
-    return axios.get(`/check-duplicate?userId=${userId}`)
-}
+    return axios.get(`/api/check-duplicate?userId=${userId}`);
+};
 
 // 이메일 중복 확인
 export const checkDuplicateEmail = (userEmail) => {
-    return axios.get(`/check-duplicate-email?userEmail=${userEmail}`)
-}
+    return axios.get(`/api/check-duplicate-email?userEmail=${userEmail}`);
+};
 
 // 아이디 찾기 처리
 export const findId = (userDetails) => {
-    return axios.post('/findId', userDetails)
+    return axios.post('/api/findId', userDetails)
         .then(response => {
-            return response.data // Should return data sent by the backend
+            return response.data; // Should return data sent by the backend
         })
         .catch(error => {
-            console.error('아이디 찾기 처리 중 오류 발생!!', error)
-            throw error
-        })
-}
+            console.error('아이디 찾기 처리 중 오류 발생!!', error);
+            throw error;
+        });
+};
 
 // 아이디 찾기 완료
 export const findIdComplete = (userId) => {
-    return axios.get(`/findIdComplete?userId=${userId}`)
+    return axios.get(`/api/findIdComplete?userId=${userId}`)
         .then(response => {
-            return response.data
+            return response.data;
         })
         .catch(error => {
-            console.error('아이디 찾기 완료 중 오류 발생!! :', error)
-            throw error
-        })
-}
+            console.error('아이디 찾기 완료 중 오류 발생!! :', error);
+            throw error;
+        });
+};
 
 // 인증 코드 전송
 export const sendAuthCode = (userEmail) => {
-    return axios.post('/sendAuthCode', { userEmail })
+    return axios.post('/api/sendAuthCode', { userEmail })
         .then(response => response.data)
         .catch(error => {
-            console.error('인증 코드 전송 중 오류 발생!!', error)
-            throw error
-        })
-}
+            console.error('인증 코드 전송 중 오류 발생!!', error);
+            throw error;
+        });
+};
 
 // 인증 코드 검증
 export const verifyAuthCode = (data) => {
-    return axios.post('/verifyAuthCode', data)
+    return axios.post('/api/verifyAuthCode', data)
         .then(response => response.data)
         .catch(error => {
-            console.error('인증 코드 검증 중 오류 발생!!', error)
-            throw error
-        })
-}
+            console.error('인증 코드 검증 중 오류 발생!!', error);
+            throw error;
+        });
+};
 
 // 비밀번호 재설정
 export const resetPassword = (userId, userPassword, confirmPassword) => {
@@ -79,28 +79,15 @@ export const resetPassword = (userId, userPassword, confirmPassword) => {
         userId: userId,
         userPassword: userPassword,
         confirmPassword: confirmPassword
-    }
+    };
 
-    return axios.post('/resetPw', data)
+    return axios.post('/api/resetPw', data)
         .then(response => response.data)
         .catch(error => {
-            console.error('비밀번호 재설정 중 오류 발생!!', error)
-            throw error
-        })
-}
+            console.error('비밀번호 재설정 중 오류 발생!!', error);
+            throw error;
+        });
+};
 
-// 비밀번호 재설정
-export const resetPw = (userId, userPassword, confirmPassword) => {
-    const data = {
-        userId: userId,
-        userPassword: userPassword,
-        confirmPassword: confirmPassword
-    }
-
-    return axios.post('/resetPw', data)
-        .then(response => response.data)
-        .catch(error => {
-            console.error('비밀번호 재설정 중 오류 발생!!', error)
-            throw error
-        })
-}
+// 비밀번호 재설정 (중복 제거)
+export const resetPw = (userId, userPassword, confirmPassword) => resetPassword(userId, userPassword, confirmPassword);
